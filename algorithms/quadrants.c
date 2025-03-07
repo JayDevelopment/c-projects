c76e5d99c9a6962db9047522b86e44d39fa4f8f9
Jay Merrill <amaninthought@gmail.com>
2025-03-07 19:30:21 +0100

    init commit for quadrants solution

---
+++ algorithms/quadrants.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int getQuadrant(int x, int y) {
+    if (x > 0 && y > 0) {
+        return 1;
+    } else if (x < 0 && y > 0) {
+        return 2;
+    } else if (x < 0 && y < 0) {
+        return 3;
+    } else if (x > 0 && y < 0) {
+        return 4;
+    } else {
+        return -1;
+    }
+}
+
+int main(void)
+{
+    // Example 1:
+    int x = 1;
+    int y = 1;
+    printf("(%d, %d) ➞ %d\n", x, y, getQuadrant(x, y)); // Expected: 1
+
+    // Example 2:
+    x = -1;
+    y = 1;
+    printf("(%d, %d) ➞ %d\n", x, y, getQuadrant(x, y)); // Expected: 2
+
+    // Example 3:
+    x = -1;
+    y = -1;
+    printf("(%d, %d) ➞ %d\n", x, y, getQuadrant(x, y)); // Expected: 3
+
+    // Example 4:
+    x = 1;
+    y = -1;
+    printf("(%d, %d) ➞ %d\n", x, y, getQuadrant(x, y)); // Expected: 4
+
+    // Example 5:
+    x = 0;
+    y = 0;
+    printf("(%d, %d) ➞ %d (x and/or y equal 0)\n", x, y, getQuadrant(x, y)); // Expected: x and/or y equal 0
+
+    return 0;
+}
